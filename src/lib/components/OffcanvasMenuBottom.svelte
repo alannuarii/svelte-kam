070981c063a31ab626a38dfd77b2b1c368295931
sierrawebid <sierrawebid@gmail.com>
2023-07-05 04:51:10 +0800

    Update

--- src/lib/components/OffcanvasMenuBottom.svelte
+++ src/lib/components/OffcanvasMenuBottom.svelte
@@ -1,3 +1,28 @@
+<script>
+	const menus = [
+		{
+			name: 'Laporan Patroli',
+			path: '/patroli/laporan',
+			icon: 'bi-arrow-repeat'
+		},
+		{
+			name: 'Laporan Apel',
+			path: '/apel/laporan',
+			icon: 'bi-people'
+		},
+		{
+			name: 'Laporan CCTV',
+			path: '/cctv/laporan',
+			icon: 'bi-camera-video'
+		},
+		{
+			name: 'Laporan Penerimaan Tamu',
+			path: '/buku-tamu/laporan',
+			icon: 'bi-person-square'
+		}
+	];
+</script>
+
 <div
 	class="offcanvas offcanvas-bottom"
 	tabindex="-1"
@@ -5,8 +30,35 @@
 	aria-labelledby="offcanvasBottomLabel"
 >
 	<div class="offcanvas-header">
-		<h5 class="offcanvas-title" id="offcanvasBottomLabel">Offcanvas bottom</h5>
-		<button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" />
+		<!-- <h5 class="offcanvas-title" id="offcanvasBottomLabel">Menu</h5> -->
+		<button
+			type="button"
+			class="btn-close ms-auto"
+			data-bs-dismiss="offcanvas"
+			aria-label="Close"
+		/>
 	</div>
-	<div class="offcanvas-body small">...</div>
+	<div class="offcanvas-body small">
+		<div class="list-group list-group-flush">
+			{#each menus as menu}
+				<a href={menu.path} class="list-group-item list-group-item-action"
+					><i class="{menu.icon} me-2" /> {menu.name}</a
+				>
+			{/each}
+		</div>
+	</div>
 </div>
+
+<style>
+	a {
+		text-decoration: none;
+		font-size: 17px;
+	}
+	.offcanvas {
+		border-radius: 20px 20px 0 0;
+		height: 350px;
+	}
+	.offcanvas .offcanvas-body {
+		overflow: hidden;
+	}
+</style>
