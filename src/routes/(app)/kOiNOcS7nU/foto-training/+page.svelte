e5d849fcc467f76e1c58e88a2db961c3f1d61ab0
sierrawebid <sierrawebid@gmail.com>
2023-07-10 16:19:45 +0800

    Update

--- src/routes/(app)/kOiNOcS7nU/foto-training/+page.svelte
+++ src/routes/(app)/kOiNOcS7nU/foto-training/+page.svelte
@@ -1,32 +1,34 @@
 <script>
-    import {API_ENDPOINT} from '../../../../lib/js/endpoint'
-    import ZoomDelete from '../../../../lib/components/ZoomDelete.svelte';
-    export let data
+	import { API_ENDPOINT } from '../../../../lib/js/endpoint';
+	import ZoomDelete from '../../../../lib/components/ZoomDelete.svelte';
+	export let data;
 
-
-    const photos = data.data
-    // const photo = photos[0].url
-
-    // console.log(`${API_ENDPOINT}/${photo}`)
+	const photos = data.data.length > 0 ? data.data : [];
 </script>
 
 <div class="px-3">
-    <div class="row gx-3 gy-3">
-        {#each photos as photo}
-        <div class="col-6">
-            <img src="{`${API_ENDPOINT}${photo.url}`}" class="img-fluid" data-bs-toggle="modal" data-bs-target="#{photo.filename}" alt="">
-            <ZoomDelete image={`${API_ENDPOINT}${photo.url}`} id={photo.filename} url={photo.url}/>
-            <p class="text-center fst-italic">{photo.label}</p>
-        </div>
-        {/each}
-    </div>
+	<div class="row gx-3 gy-3">
+		{#each photos as photo}
+			<div class="col-6">
+				<img
+					src={`${API_ENDPOINT}${photo.url}`}
+					class="img-fluid"
+					data-bs-toggle="modal"
+					data-bs-target="#{photo.filename}"
+					alt=""
+				/>
+				<ZoomDelete image={`${API_ENDPOINT}${photo.url}`} id={photo.filename} url={photo.url} />
+				<p class="text-center fst-italic">{photo.label}</p>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
-    img{
-        border-radius: 20px;
-    }
-    p{
-        margin: 0px;
-    }
+	img {
+		border-radius: 20px;
+	}
+	p {
+		margin: 0px;
+	}
 </style>
